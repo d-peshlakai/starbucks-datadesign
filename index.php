bde5502829985b85c6fde1e2fb1a99cf83f51d3e
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-15 12:20:30 -0600

    adding content

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<htmL>
+	<title>
+		Starbucks Datadesign
+	</title>
+	<body>
+		<h1>
+			Persona
+		</h1>
+		<p>The site assigned was Starbucks web page. The user purpose would be a non-member 		customer purchasing either espresso coffee whole bean or the Veriso pods.</p>
+		<h2>Interaction Flow</h2>
+		<p>The goal would be to satisfy their taste buds by purchasing a specific flavor coffee. The user would start off with</p>
+		<ol>
+			<li>
+				Entering URL site
+			</li>
+			<li>
+				Clicking on category titled "Coffee"
+			</li>
+			<li>
+				Selecting from choice from category "Whole Bean and Ground"
+			</li>
+			Entering URL site
+		</ol>
+	</body>
+</htmL>
