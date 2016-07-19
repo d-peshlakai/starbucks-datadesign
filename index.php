993acfae2aa035195bfc1dc65127270e3dae4be6
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-18 21:24:23 -0600

    revision on conceptual model

--- index.php
+++ index.php
@@ -8,8 +8,16 @@
 			Persona
 		</h1>
 		<p>The site assigned was Starbucks web page. The user is a 26 year old college student who loves coffee. The purpose would be for a usual customer purchasing the Verismo pods.</p>
+		<h1>Use Case</h1>
+		<p>Goal: As a Starbucks buyer, she want the pods.</p>
+		<ol>
+			<li>What: Use of the Windows 7 computer.
+			</li>
+			<li>Who: Customer who loves coffee and needs it daily to function. Her favorite coffee type form of choice for coffee maker.</li>
+			<li>Why: She want to buy her favorite tasting house blend coffee. It reflects off of the customers persona as a coffee lover.</li>
+		</ol>
 		<h1>Interaction Flow</h1>
-		<p>The goal would be to satisfy the customers taste buds by purchasing a specific flavor coffee including mapping out the data at rest. The logical containers would involve: order, customer, and product, and vendor.  The user would start off in the following steps:</p>
+		<p>The goal would be to satisfy the customer and mapping out the data at rest. The logical containers would involve: customer, order and product, and vendor.  The user would start off in the following steps to purchase the coffee of choice:</p>
 		<ol>
 			<li>
 				Entering URL site
@@ -25,27 +33,20 @@
 			<li>Add choice to checkout cart link</li>
 			<li>Enter billing information and purchasing information.</li>
 		</ol>
-		<h1>Use Case</h1>
-		<p>Goal: As a Starbucks buyer, she want the pods.</p>
-		<ol>
-			<li>What: Use of the Windows 7 computer.
-			</li>
-			<li>Who: Customer satisfying the taste buds by buying favorite coffee for coffee maker.</li>
-			<li>Why: I want to buy my favorite tasting coffee. It reflects off of the customers persona as a coffee lover.</li>
 
-		</ol>
 		<h1>Conceptual Model</h1>
 		<p>
 			Relationships
 			<ol>
-				<p>One to many: entry can have many entities. customerId would be the primary key and productId would be a foreign key, type of roast would be another foreign key to give customer a choice, and number of stars would rate the type of coffee chosen.</p>
+				<p>One to many: entry can have many entities. customerFe would be the primary key and productId would be a foreign key.</p>
+			<li>Description to identity the coffee of choice</li>
 			<li>
 				Selection of purchasing Pods or whole ground bean
 			</li>
 			<li>
 				Selection of type of roast: Dark, Blonde, or Medium
 			</li>
-			<li>Also choosing from decaffeinated or caffeinated type.</li>
+			<li>Choice of decaffeinated or caffeinated type.</li>
 			<li>
 				Option to share on another social media source like Pinterest.
 			</li>
@@ -54,6 +55,6 @@
 			<li>The reviews can entail which top product is a favorite by other customers</li>
 			</ol>
 		</p>
-		<p> A weak entity would be when the user buys more than one type of coffee. </p>
+		<p> A weak entity would be when the user buys more than one type of coffee and </p>
 	</body>
 </htmL>
