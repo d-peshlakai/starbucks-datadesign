ca646b9b333093ec4ef29a103803b47b02396a62
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-20 18:43:48 -0600

    update datadesign and erd

--- index.php
+++ index.php
@@ -9,15 +9,15 @@
 		</h1>
 		<p>The site assigned was Starbucks web page. The user is a 26 year old college student who loves coffee. The purpose would be for a usual customer purchasing the Verismo pods.</p>
 		<h1>Use Case</h1>
-		<p>Goal: As a Starbucks buyer, she want the pods.</p>
+		<p>Goal: As a Starbucks buyer, she want the pods for her coffee maker.</p>
 		<ol>
 			<li>What: Use of the Windows 7 computer.
 			</li>
-			<li>Who: Customer who loves coffee and needs it daily to function. Her favorite coffee type form of choice for coffee maker.</li>
-			<li>Why: She want to buy her favorite tasting house blend coffee. It reflects off of the customers persona as a coffee lover.</li>
+			<li>Who: Customer who loves coffee and needs it daily to function. Her favorite coffee type form of choice for coffee maker to her convenience.</li>
+			<li>Why: She want to buy her favorite tasting house blend coffee. It reflects off of the customers persona as a coffee lover and shows she wants something fast and easy.</li>
 		</ol>
 		<h1>Interaction Flow</h1>
-		<p>The goal would be to satisfy the customer and mapping out the data at rest. The logical containers would involve: customer, order and product, and vendor.  The user would start off in the following steps to purchase the coffee of choice:</p>
+		<p>The goal would be to satisfy the customer's preference and mapping out the data at rest. The logical containers would involve: customer, order and product, and vendor.  The user would start off in the following steps to purchase the coffee of choice:</p>
 		<ol>
 			<li>
 				Entering URL site
@@ -38,24 +38,14 @@
 		<p>
 			Relationships
 			<ol>
-				<p>One to many: entry can have many entities. customerFe would be the primary key and productId would be a foreign key.</p>
-			<li>Customer selects destination</li>
-			<li>
-				
+		<p>One to many: entry can have many entities. customerFe would be the primary key and starRating would be a foreign key.</p>
+			<li>CustomerF can buy many pods which reflects the one to many relation</li>
+			<li>Many to many relation would have the starRating of customers be the weak entity.
 			</li>
+			<li>The weak entity would store the other user under nameId, locationId, and numberStar. </li>
 			<li>
-				Selection of type of roast: Dark, Blonde, or Medium
+				roastType and coffeeId would be the attributes of the pods.
 			</li>
-			<li>Choice of decaffeinated or caffeinated type.</li>
-			<li>
-				Option to share on another social media source like Pinterest.
-			</li>
-			<li>Customer can also distinguish where espresso is from and which multi region it is derived from.</li>
-			<li>Also select the particular quantity wanting to purchased.</li>
-			<li>The reviews can entail which top product is a favorite by other customers</li>
-			</ol>
-		</p>
-		<p> A weak entity would be when the user buys more than one type of coffee and </p>
 		<img src="pics/starbucks.png" alt="starbucks"/>
 
 	</body>
