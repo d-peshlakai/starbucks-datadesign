d17a8decdd76e945bf0d6879712a8d9028350f9f
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-23 12:04:07 -0600

    edited more info/pic

--- index.php
+++ index.php
@@ -7,17 +7,17 @@
 		<h1>
 			Persona
 		</h1>
-		<p>The site assigned was Starbucks web page. The user is a 26 year old college 			student who loves coffee. The site is designed to sell coffee products.<br> The 			purpose would be a usual customer purchasing the Verismo pods on an late 			afternoon day.</p>
+		<p>The site assigned was Starbucks web page. The user is a 26 year old 			college student who loves coffee. The site is designed to sell coffee products  The 			purpose would be a usual customer purchasing the Verismo pods on an late 			afternoon day.</p>
 		<h1>Use Case</h1>
 		<p>Goal: As a Starbucks buyer, she want the pods for her coffee maker.</p>
 		<ol>
 			<li>What: Use of the Windows 7 computer.
 			</li>
-			<li>Who: Customer who loves coffee and needs it daily to function. Her favorite coffee type form of choice for coffee maker to her convenience.</li>
-			<li>Why: She want to buy her favorite tasting house blend coffee. It reflects off of the customers persona as a coffee lover and shows she wants something fast and easy.</li>
+			<li>Who: Customer who loves coffee and needs it daily to function. Her favorite 				coffee type form of choice for coffee maker to her convenience.</li>
+			<li>Why: She want to buy her favorite tasting house blend coffee. It reflects 				off of the customers persona as a coffee lover and shows she wants something 				fast and easy.</li>
 		</ol>
 		<h1>Interaction Flow</h1>
-		<p>The goal would be to satisfy the customer's preference and mapping out the data at rest. The logical containers would involve: customer, order and product, and vendor.  The user would start off in the following steps to purchase the coffee of choice:</p>
+		<p>The goal would be to satisfy the customer's preference and mapping out the data 			at rest. The logical containers would involve: customer, order and product, and 			vendor.  The user would start off in the following steps to purchase the coffee 			of choice:</p>
 		<ol>
 			<li>
 				Entering URL site
@@ -38,16 +38,15 @@
 		<p>
 			Relationships
 			<ol>
-		<p>One to many: entry can have many entities. customerFe would be the primary key and starRating would be a foreign key.</p>
-			<li>CustomerF can buy many pods which reflects the one to many relation</li>
-			<li>Many to many relation would have the starRating of customers be the weak entity.
+			<p>One to many: entry can have many entities. customerFe would be the primary 			key and starRating would be a foreign key.</p>
+			<li>CustomerF can buy many pods which reflects the one to many relation It's attributes contains the pods and the number of quantity of pods.</li>
+			<li>Many to many relation would have the starRating of customers be the weak 				entity.
 			</li>
-			<li>The weak entity would store the other user under nameId, locationId, and numberStar. </li>
+			<li>The weak entity would be the starRating that opens many entities under 				nameId, locationId, and numberStar. </li>
 			<li>
 				roastType and coffeeId would be the attributes of the pods.
 			</li>
-		<img src="pics/starbucks.png" alt="starbucks"/>
+			<img src="pics/erd.png" alt="starbucks"/>
 
 	</body>
-
 </htmL>
---
+++ php/classes/coffee.php
@@ -0,0 +1,7 @@
+<?php
+
+ ?>Created by PhpStorm.
+ * User: BackInUse
+ * Date: 7/23/2016
+ * Time: 11:19 AM
+ */
