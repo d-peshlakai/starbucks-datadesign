100320a811c629e028157ff36930c9fc5b11f529
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-25 20:31:24 -0600

    final check/proofread

--- index.php
+++ index.php
@@ -5,32 +5,27 @@
 	<title>
 		Starbucks Datadesign
 	</title>
+	<h1>Starbucks-Datadesign</h1>
 	<body>
-		<h1>
+		<h2>
 			Persona
-		</h1>
-		<p>The site assigned was Starbucks web page. The user is a 26 year old 	college 	student who loves coffee. The site 			is designed to purchase Starbucks products.  The purpose would be the coffee drinker to buy her favorite coffee and 			to rate the particular coffee an integer from 1-5, having 5 being the highest and most favorite. </p>
-		<h1>Use Case</h1>
+		</h2>
+		<p>The site assigned was Starbucks web page. The customer is a 26 year old college 	student who loves coffee. The site 	is designed to purchase Starbucks products.  The purpose would be the coffee drinker to buy her favorite coffee and to rate the particular coffee an integer from 1-5, having 5 being the highest and most favorite. </p>
+		<h2>Use Case</h2>
 		<p>Goal: As a Starbucks buyer and coffee drinker, she is rating her top favorite coffee  product by number of stars.</p>
 		<ol>
 			<li>What: Use of the Windows 7 computer desktop.
 			</li>
 			<li>Who: A female customer who loves coffee and needs it daily to function has a choice of the many item types, the bag 				count, price, roast type, and the 	identification of the highly rated product of coffee.</li>
-			<li>Why: The coffee drinker rates her most favorite coffee, and from there forms an entity for coffee. It reflects 				off of the customers persona as a coffee lover and shows she wants something 	fast and easy.</li>
+			<li>Why: The coffee drinker rates her most favorite coffee, and from there forms an entity for coffee. It reflects 				off of the customers persona as a coffee lover and shows she admires the coffee product.</li>
 		</ol>
 
-		<h1>Interaction Flow</h1>
-		<p>The goal would the female buyer who chooses to rate her favorite product. The logical containers would involve: customer, order and product, and vendor.  The user would start off in the following steps to purchase the coffee of choice:</p>
+		<h2>Interaction Flow</h2>
+		<p>The goal is the female buyer who chooses to rate her favorite product. The logical containers would involve: customer, order and product, and vendor.  The customer would start off in the following steps to purchase the coffee of choice:</p>
 		<ol>
-			<li>
-				Entering URL site
-			</li>
-			<li>
-				Clicking on category titled "Coffee"
-			</li>
-			<li>
-				Selecting from category the coffee product of choice.
-			</li>
+			<li>Entering URL site</li>
+			<li>Clicking on category titled "Coffee"</li>
+			<li>Selecting from category the coffee product of choice.</li>
 			<li>Select the certain quantity.</li>
 			<li>Select one of the options: blonde, medium, or dark</li>
 			<li>Select the specified description.</li>
@@ -39,7 +34,7 @@
 			<li>Customer rates the most favorite coffee product.</li>
 		</ol>
 
-		<h1>Conceptual Model</h1>
+		<h2>Conceptual Model</h2>
 		<p>
 			Relationships
 			<ol>
