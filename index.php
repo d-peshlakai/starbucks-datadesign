dc381768be628b068649726223722ca8ff1abec6
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-25 20:24:43 -0600

    edited and revised each paragraph

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <htmL>
+
+	<meta charset="utf-8">
 	<title>
 		Starbucks Datadesign
 	</title>
@@ -7,17 +9,18 @@
 		<h1>
 			Persona
 		</h1>
-		<p>The site assigned was Starbucks web page. The user is a 26 year old 			college student who loves coffee. The site is designed to sell coffee products  The 			purpose would be a usual customer purchasing the Verismo pods on an late 			afternoon day.</p>
+		<p>The site assigned was Starbucks web page. The user is a 26 year old 	college 	student who loves coffee. The site 			is designed to purchase Starbucks products.  The purpose would be the coffee drinker to buy her favorite coffee and 			to rate the particular coffee an integer from 1-5, having 5 being the highest and most favorite. </p>
 		<h1>Use Case</h1>
-		<p>Goal: As a Starbucks buyer, she want the pods for her coffee maker.</p>
+		<p>Goal: As a Starbucks buyer and coffee drinker, she is rating her top favorite coffee  product by number of stars.</p>
 		<ol>
-			<li>What: Use of the Windows 7 computer.
+			<li>What: Use of the Windows 7 computer desktop.
 			</li>
-			<li>Who: Customer who loves coffee and needs it daily to function. Her favorite 				coffee type form of choice for coffee maker to her convenience.</li>
-			<li>Why: She want to buy her favorite tasting house blend coffee. It reflects 				off of the customers persona as a coffee lover and shows she wants something 				fast and easy.</li>
+			<li>Who: A female customer who loves coffee and needs it daily to function has a choice of the many item types, the bag 				count, price, roast type, and the 	identification of the highly rated product of coffee.</li>
+			<li>Why: The coffee drinker rates her most favorite coffee, and from there forms an entity for coffee. It reflects 				off of the customers persona as a coffee lover and shows she wants something 	fast and easy.</li>
 		</ol>
+
 		<h1>Interaction Flow</h1>
-		<p>The goal would be to satisfy the customer's preference and mapping out the data 			at rest. The logical containers would involve: customer, order and product, and 			vendor.  The user would start off in the following steps to purchase the coffee 			of choice:</p>
+		<p>The goal would the female buyer who chooses to rate her favorite product. The logical containers would involve: customer, order and product, and vendor.  The user would start off in the following steps to purchase the coffee of choice:</p>
 		<ol>
 			<li>
 				Entering URL site
@@ -26,26 +29,26 @@
 				Clicking on category titled "Coffee"
 			</li>
 			<li>
-				Selecting from catergory of the Pods form.
+				Selecting from category the coffee product of choice.
 			</li>
 			<li>Select the certain quantity.</li>
 			<li>Select one of the options: blonde, medium, or dark</li>
+			<li>Select the specified description.</li>
 			<li>Add choice to checkout cart link</li>
 			<li>Enter billing information and purchasing information.</li>
+			<li>Customer rates the most favorite coffee product.</li>
 		</ol>
 
 		<h1>Conceptual Model</h1>
 		<p>
 			Relationships
 			<ol>
-			<p>One to many: entry can have many entities. customerFe would be the primary 			key and starRating would be a foreign key.</p>
-			<li>CustomerF can buy many pods which reflects the one to many relation It's attributes contains the pods and the number of quantity of pods.</li>
-			<li>Many to many relation would have the starRating of customers be the weak 				entity.
+		<p>The important data models required would be the customer to reveal the essential data about the customer. Coffee containing attributes of description, type item, roast, count, price and name. All these to obtain coffeeId as the primary key. When coffee is then rated by the customer. The action to rate opens a many to many relationship with the weak entity of starRating. Then leaving the starRatingCustomerId and starRatingCoffeeId as the foreign keys. They are the collection/list of identifying the customers leaving the rates and identifying the highest rate coffee product. </p>
+
+			<li>Customer can rate the coffee product he/she likes from 0 to 5 with 5 being the most liked.</li>
+			<li>Many to many relation would have the starRating of customers the weak 				entity.
 			</li>
-			<li>The weak entity would be the starRating that opens many entities under 				nameId, locationId, and numberStar. </li>
-			<li>
-				roastType and coffeeId would be the attributes of the pods.
-			</li>
+			<li>The weak entity would be the starRating that links the customer and coffee together. It also opens foreign keys star rating of customer and star rating the coffee identification. </li>
 			<img src="pics/erd-star.png" alt="starbucks"/>
 
 	</body>
--- php/classes/coffee.php
+++ php/classes/coffee.php
@@ -3,14 +3,25 @@
  * accessor method for Starbucks inputCustomerId
  *@return  int value of Starbucks inputCustomerId
  **/
- public function getinputCustomerId () {
+ private function getinputCustomerId () {
   return($this->inputCustomerId);
 }
 
 /**
  * mutator method for Starbucks input Customer Id
  *
- * @param int $newCustomerId new value of tweet profile id
+ * @param int $newCustomerId new value of customer profile id
  * @throws \RangeException if $newCustomerId is negative
- * @throw
+ * @throws \IdError if $newCustomerId is not an integer
+ **///
+ 	private function setNewCustomerId (int
+	$newCustomerId) {
+	// verify that the customer id is positive
+		if ($newCustomerId !==0) {
+			throw(new\OutOfRangeExceptionException ("customer name is not found"));
+		}
+	//store the new customer id
+	$this->customerId = $newCustomerId;
+}
+
 ?>
