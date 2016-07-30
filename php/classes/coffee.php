5b54b3326872e07a5417979aee6e84803d1a7cd0
Diane Peshlakai <dpeshlakai3@cnm.edu>
2016-07-29 23:49:48 -0600

    accessor/mutator method edit

--- php/classes/coffee.php
+++ php/classes/coffee.php
@@ -1,42 +1,213 @@
 <?php
-/**
- * accessor method for Starbucks getCustomerId
- *@return  int value of Starbucks getCustomerId
- **/
- private function getCoffeeId () {
-  return($this->coffeeId);
-}
+
 
 /**
- * mutator method for Starbucks input Customer Id
+ * @author Diane Peshlakai
+ */
+class Coffee implements {
+	/*
+	 * id for coffee id, coffee id is the primary key
+	 * var int $coffeeid
+	 */
+	private $coffeeId;
+	/*
+	 * @var name of different type of coffee
+	 */
+	private $coffeeName;
+	/*
+	 * @var name of coffee price
+	 */
+	private $coffeePrice;
+	/*
+	 * @var name of coffee description
+	 */
+	private $coffeeDescription;
+	/*
+	 * @var name of coffee item type: pods or ground coffee
+	 */
+	private $coffeeItemType;
+	/*
+	 * @var name of coffee count quantity
+	 */
+	private $coffeeCount;
+	/*
+	 * @var name of coffee roast
+	 */
+	private $coffeeRoast;
+
+
+/**
+ *  constructor of the coffee roast id
+ * @param int|null $newCoffeeRoast id of correct coffee name or null if wrong name
+ *@param int | null $coffeeDescription id to identify the product or null if doesnt match
+ * @param int| null $coffeeItemtype id to identify the product or null if unknownproduct
+ *///
+public function __construct (int $newCoffeeName = null, int $newCoffeePrice = null, string $newCoffeeDescription, $newCoffeeItemType = null, $newCoffeeCount = null, $newCoffeeRoast = null) {
+
+try {
+	$this->seCcoffeeId($newCustomerId);
+	$this->setCoffeeName($newCoffeeName);
+	$this->setCoffeePrice($newCoffeePrice);
+	$this->setCoffeeDescription($newCoffeeDescription);
+	$this->setCoffeeItemType($newCoffeeItemType);
+	$this->setCoffeeCount($newCoffeeCount);
+	$this->setCoffeeRoast($newCoffeeRoast);
+}
+catch(\InvalidArgumentException $invalidArgument) {
+	throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+}
+catch(\InvalidException $invalidException) {
+	throw(new \InvalidException $invalidException($invalidException->getMessage(), 0, $invalidException));
+}
+catch(\RangeException $range) {
+	throw(new \RangeException ($range->getMessage(),0, $range));
+}
+catch (\TypeError $typeError) {
+	throw(new \TypeError ($typeError->getMessage(), 0, $typeError));
+}
+catch (\InvalidArgumentException $invalidargument) {
+	throw(new \InvalidArgumentException ($invalidException->getMessage(), 0, $invalidException));
+}
+catch (\RangeException $range) {
+	throw(new \RangeException ($range->getMessage(), 0, $range));
+}
+catch (\Exception $exception) {
+	throw(new\Exception ($exception->getMessage(), 0, $exception));
+}
+
+/*
+ * accessor method for coffee id 
+ * @return int null value of coffee id 
+ */
+public function getCoffeeId() {
+	return($this->coffeeId);
+}
+/*
+ * mutator method for coffee id 
  *
- * @param int $ new value of customer profile id
- * @throws \RangeException if $newCustomerId is negative
- * @throws \IdError if $newCustomerId is not an integer
- **///
- 	public function setNewCoffeeId (int
-	$newCoffeeId) {
-	// verify that the customer id is positive
-		if ($newCoffeeId !==0) {
-			throw(new\OutOfRangeException ("coffee name is not found"));
-		}
-	//store the new coffee id
+ * @param int null $newCoffeeId new value of coffee
+ * @throws \TypeError if $newCoffeeId doesn't exist
+ */
+public function setCoffeeId(int $newCoffeeId = null) {
+	//if the coffee is null this is a new coffee name
+	if($newCoffeeId === null) {
+		$this->coffeeId = null;
+		return;
+	}
+	// verify that coffee id is positive
+	if($newCoffeeId <= 0) {
+		throw(new\InvalidArgumentException("coffee id is not positive"));
+	}
+	// convert and store the coffee id
 	$this->coffeeId = $newCoffeeId;
 }
 
-/**
- *  constructor of the coffee Id
- * @param int|null $newCoffee id of correct coffee name or null if wrong name
- *@param int
- **///
-	public function _construct (int $newCustomerId = null) {
-	try {
-		$this->setCustomerId($newCustomerId);
-		$this->setCoffeeDescription($newCoffeeDescription);
-		$this->setCoffeeItemType($newCoffeeType);
-	}
-	catch(\invalidArgumentException $invalidArgument) {
-		throw(new \InvalidArgumentException($invaldiArgument->getCoffeeId))
-	}
+// accessor method for coffee name
+//@return int value of coffee name
+/
+public function getCoffeeName() {
+	return ($this->coffeeName);
 }
-?>
+//* mutator method for coffee name
+// @param int null new value of coffee name
+//@throws \InvalidException if $newCoffeeName is not positive
+/**@throws /TypeError \TypeError if @newCoffeeName is not an integer
+*/
+public function setCoffeeName(int $newCoffeeName)
+
+			//verify the coffee name is positive
+			if ($newCoffeeName !== 0) {
+		throw new \InvalidArgumentException("coffee name is not positve")
+				};
+		//convert and store the coffeeNAme
+	$this->coffeeName = $newCoffeeName;
+}
+
+//*Accessor method for coffee price
+/**@return int value of coffee price
+ *//
+public function getCoffeePrice() {
+	return($this->coffeePrice);
+}
+/* mutator method for coffee name
+' @param in $newCofeePrice new value of coffee shops
+*//
+public function setCoffeePrice(int $newCoffeePrice) {
+	//verify that coffee price is not positive
+	if ($newCoffeePrice <=0) {
+		throw(new\RangeException("coffee is invalid"));
+	}//convert and store the coffee name
+		$this->coffeePrice= $newCoffeePrice;
+}
+//*accessor method for coffee description
+// @return string value of coffee description
+public function getCoffeeDescription() {
+	return($this->coffeeDescription);
+}
+
+/**mutator method for coffee description
+ *
+ * @param string $newCoffeeDescription new value of coffee content
+ * @throws \TypeError if $newCoffeeDescription is not a string or insecure
+ * */
+public function setCoffeeDescription(string $newcoffeeDescription) {
+	//verify the coffee description is secure
+	$newCoffeeDescription = trim($newcoffeeDescription);
+	$newcoffeeDescription = filter_var($newcoffeeDescription, FILTER_SANITIZE_STRING);
+	if(empty($newcoffeeDescription)==true) {
+	throw(new\InvalidArgumentException("coffee description is empty or insecure"));
+	}
+	//store the tweet content
+	$this->coffeeDescription=$newCoffeeDescription;
+}
+/**
+ *accessor method for coffee item type
+ * @return int value of coffee item type
+ */
+public function getcoffeeItemType() {
+	return($this->coffeeItemType);
+}
+/**
+ * mutator method for coffee item type
+ * @param int $newCoffeeItemType new value of coffeeId
+ * @throws \RangeException if coffee id is not positive
+ * @throws \TypeError if $newCoffeeItemType is not an integer
+ */
+public function setcoffeeItemType(int $newcoffeeItemType) {
+	//verify if coffee item is positive
+	if($newcoffeeItemType <= 0) {
+	throw(new \RangeException("coffee item is not positive"));
+	}
+	//convert and store the coffee item type
+	$this->coffeeItemType = $newcoffeeItemType;
+}
+/*
+ * accessor method for coffee count
+ * @return float value of coffee count
+ */
+public function getcoffeeCount() {
+		return($this->coffeeCount)
+}
+/*
+ * mutator method for coffee count
+ * @param float value of coffee count
+ * @throws \RangeException if coffee count is not positive
+ */
+public function setcoffeeCount(float $newcoffeeCount) {
+		//verify if float is positive
+			if($newcoffeeCount <=0 ) {
+				throw(new \RangeException("coffee count is not positive"));
+			}
+		$this->coffeeCount =$newcoffeeCount;
+		}private function setCoffeeName($newCoffeeName) {
+}
+
+
+
+
+
+
+
+
+
+
--- sql/starbucks.sql
+++ sql/starbucks.sql
@@ -9,27 +9,27 @@ CREATE TABLE customer(
 	customerHash CHAR(128) NOT NULL,
 	customerEmail VARCHAR(128) NOT NULL,
 	UNIQUE (customerEmail),
+	INDEX (customerId),
 	PRIMARY KEY (customerId)
 );
 
 CREATE TABLE coffee (
 	coffeeId          INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	coffeeName        INT UNSIGNED                NOT NULL,
+	coffeeName        VARCHAR(128) NOT NULL,
 	coffeePrice       DECIMAL(2, 2),
 	coffeeDescription VARCHAR(148)                NOT NULL,
 	coffeeItemType    VARCHAR(34)                 NOT NULL,
 	coffeeCount       DECIMAL(5, 2),
 	coffeeRoast       VARCHAR(32)                 NOT NULL,
-	INDEX (coffeeName),
-	FOREIGN KEY (coffeeItemType) REFERENCES customer (customerId),
+	INDEX (coffeeId),
 	PRIMARY KEY (coffeeId)
 );
 
 CREATE TABLE starRating (
+	starRatingCoffeeId INT UNSIGNED NOT NULL,
 	starRatingCustomerId INT UNSIGNED NOT NULL,
 	starDate DATETIME NOT NULL,
 	starRating SMALLINT UNSIGNED,
-	starRatingCoffeeId VARCHAR(128),
 	INDEX (starRatingCoffeeId),
 	INDEX (starRatingCustomerId),
 	FOREIGN KEY (starRatingCoffeeId) REFERENCES coffee(coffeeId),
